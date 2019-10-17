dd3328c254df85d3eba55319325aef2c255ebb58
kezhu.wukz <kezhu.wukz@alipay.com>
2019-10-17 23:37:19 +0800

    Update UserDataElementPushTask: push empty data only once

--- server/common/model/src/main/java/com/alipay/sofa/registry/common/model/store/Subscriber.java
+++ server/common/model/src/main/java/com/alipay/sofa/registry/common/model/store/Subscriber.java
@@ -16,12 +16,13 @@
  */
 package com.alipay.sofa.registry.common.model.store;
 
-import com.alipay.sofa.registry.common.model.ElementType;
-import com.alipay.sofa.registry.core.model.ScopeEnum;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.alipay.sofa.registry.common.model.ElementType;
+import com.alipay.sofa.registry.common.model.constants.ValueConstants;
+import com.alipay.sofa.registry.core.model.ScopeEnum;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  *
@@ -31,16 +32,33 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Subscriber extends BaseInfo {
 
     /** UID */
-    private static final long                serialVersionUID = 98433360274932292L;
+    private static final long serialVersionUID = 98433360274932292L;
     /** */
-    private ScopeEnum                        scope;
+    private ScopeEnum         scope;
     /** */
-    private ElementType                      elementType;
+    private ElementType       elementType;
+
+    static class PushContext {
+        /**
+         * push dataInfo version
+         */
+        private Long lastPushVersion;
+
+        /**
+         * push dataInfo version
+         */
+        private int  lastPubCount;
+
+        public PushContext(Long lastPushVersion, int lastPubCount) {
+            this.lastPushVersion = lastPushVersion;
+            this.lastPubCount = lastPubCount;
+        }
+    }
 
     /**
      * all dataCenter push dataInfo version
      */
-    private Map<String/*dataCenter*/, Long> lastPushVersions = new ConcurrentHashMap<>();
+    private Map<String/*dataCenter*/, PushContext> lastPushContexts = new ConcurrentHashMap<>();
 
     /**
      * Getter method for property <tt>scope</tt>.
@@ -71,7 +89,11 @@ public class Subscriber extends BaseInfo {
      */
     public boolean checkVersion(String dataCenter, Long version) {
 
-        Long oldVersion = lastPushVersions.get(dataCenter);
+        PushContext pushContext = lastPushContexts.get(dataCenter);
+        if (pushContext == null) {
+            return version != null;
+        }
+        Long oldVersion = pushContext.lastPushVersion;
         if (oldVersion == null) {
             return version != null;
         } else {
@@ -88,15 +110,26 @@ public class Subscriber extends BaseInfo {
      * @return
      */
     public void checkAndUpdateVersion(String dataCenter, Long version) {
+        checkAndUpdateVersion(dataCenter, version, -1);
+    }
+
+    /**
+     * check version input greater or equal to current version
+     * @param version
+     * @return
+     */
+    public void checkAndUpdateVersion(String dataCenter, Long version, int pubCount) {
 
         while (true) {
-            Long oldVersion = lastPushVersions.putIfAbsent(dataCenter, version);
+            PushContext pushContext = new PushContext(version, pubCount);
+            PushContext oldPushContext = lastPushContexts.putIfAbsent(dataCenter, pushContext);
             // Add firstly
-            if (oldVersion == null) {
+            if (oldPushContext == null) {
                 break;
             } else {
-                if (version > oldVersion) {
-                    if (lastPushVersions.replace(dataCenter, oldVersion, version)) {
+                if (oldPushContext.lastPushVersion == null
+                    || (pushContext.lastPushVersion != null && pushContext.lastPushVersion > oldPushContext.lastPushVersion)) {
+                    if (lastPushContexts.replace(dataCenter, oldPushContext, pushContext)) {
                         break;
                     }
                 } else {
@@ -104,6 +137,22 @@ public class Subscriber extends BaseInfo {
                 }
             }
         }
+    }
+
+    /**
+     * http://gitlab.alipay-inc.com/sofa-open/sofa-registry/issues/189
+     */
+    public boolean shouldPush(String dataCenter, int pubCount) {
+        // condition of no push:
+        // 1. last push count is 0 and this time is also 0
+        // 2. last push is a valid push
+        if (pubCount == 0) {
+            PushContext pushContext = lastPushContexts.get(dataCenter);
+            return !(pushContext != null && pushContext.lastPubCount == 0
+            //last push is a valid push
+                     && pushContext.lastPushVersion != null && pushContext.lastPushVersion > ValueConstants.DEFAULT_NO_DATUM_VERSION);
+        }
+        return true;
     }
 
     /**
@@ -126,26 +175,8 @@ public class Subscriber extends BaseInfo {
         final StringBuilder sb = new StringBuilder("scope=");
         sb.append(scope).append(",");
         sb.append("elementType=").append(elementType).append(",");
-        sb.append("lastPushVersion=").append(lastPushVersions);
+        sb.append("lastPushVersion=").append(lastPushContexts);
         return sb.toString();
-    }
-
-    /**
-     * Getter method for property <tt>lastPushVersions</tt>.
-     *
-     * @return property value of lastPushVersions
-     */
-    public Map<String, Long> getLastPushVersions() {
-        return lastPushVersions;
-    }
-
-    /**
-     * Setter method for property <tt>lastPushVersions </tt>.
-     *
-     * @param lastPushVersions  value to be assigned to property lastPushVersions
-     */
-    public void setLastPushVersions(Map<String, Long> lastPushVersions) {
-        this.lastPushVersions = lastPushVersions;
     }
 
     /**
@@ -156,7 +187,7 @@ public class Subscriber extends BaseInfo {
         final StringBuilder sb = new StringBuilder("Subscriber{");
         sb.append("scope=").append(scope);
         sb.append(", elementType=").append(elementType);
-        sb.append(", lastPushVersions=").append(lastPushVersions);
+        sb.append(", lastPushContexts=").append(lastPushContexts);
         sb.append(", super=").append(super.toString());
         sb.append('}');
         return sb.toString();
