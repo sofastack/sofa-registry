73f80207925c2b0118e25e0bf6347fd7fbc088f0
kezhu.wukz <kezhu.wukz@alipay.com>
2020-05-29 19:15:00 +0800

    fix： https://github.com/sofastack/sofa-registry/issues/121

--- server/server/session/src/main/java/com/alipay/sofa/registry/server/session/store/SessionInterests.java
+++ server/server/session/src/main/java/com/alipay/sofa/registry/server/session/store/SessionInterests.java
@@ -167,6 +167,8 @@ public class SessionInterests implements Interests, ReSubscribers {
                     }
                 }
             }
+            //force remove connectId
+            invalidateConnectIndex(connectId);
             return true;
         } catch (Exception e) {
             LOGGER.error("Delete subscriber by connectId {} error!", connectId, e);
