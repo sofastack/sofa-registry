606d7d3c299066c41c6c80ca0c70710d3cd5ed81
yuzhi.lyz <yuzhi.lyz@antfin.com>
2021-01-28 16:14:29 +0800

    fix testcase

--- server/server/session/src/main/java/com/alipay/sofa/registry/server/session/store/SessionWatchers.java
+++ server/server/session/src/main/java/com/alipay/sofa/registry/server/session/store/SessionWatchers.java
@@ -47,9 +47,11 @@ public class SessionWatchers extends AbstractDataManager<Watcher> implements Wat
 
         Watcher existingWatcher = addData(watcher);
         if (existingWatcher != null) {
-            LOGGER.warn("dups watcher, {}, {}, exist={}, input={}",
-                existingWatcher.getDataInfoId(), existingWatcher.getRegisterId(),
-                existingWatcher.registerVersion(), watcher.registerVersion());
+            LOGGER.warn("dups watcher, {}, {}, exist={}/{}, input={}/{}",
+                    existingWatcher.getDataInfoId(), existingWatcher.getRegisterId(),
+                    // not use get registerVersion, avoid the subscriber.version is null
+                    existingWatcher.getVersion(), existingWatcher.getRegisterTimestamp(),
+                    watcher.getVersion(), watcher.getRegisterTimestamp());
         }
         return true;
     }
