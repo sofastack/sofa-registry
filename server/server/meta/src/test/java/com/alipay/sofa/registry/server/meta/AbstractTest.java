024bbd41f1ae31ab2735ad3322515a3fad6cbf67
bjxiaojian <hitxiaojian@163.com>
2021-02-03 12:03:47 +0800

    slot chaos

--- server/server/meta/src/test/java/com/alipay/sofa/registry/server/meta/AbstractTest.java
+++ server/server/meta/src/test/java/com/alipay/sofa/registry/server/meta/AbstractTest.java
@@ -134,6 +134,33 @@ public class AbstractTest {
         logger.info(remarkableMessage("[end test][{}]"), name.getMethodName());
     }
 
+    protected static void setEnv(Map<String, String> newenv) throws Exception {
+        try {
+            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+            Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+            theEnvironmentField.setAccessible(true);
+            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+            env.putAll(newenv);
+            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+            theCaseInsensitiveEnvironmentField.setAccessible(true);
+            Map<String, String> cienv = (Map<String, String>)theCaseInsensitiveEnvironmentField.get(null);
+            cienv.putAll(newenv);
+        } catch (NoSuchFieldException e) {
+            Class[] classes = Collections.class.getDeclaredClasses();
+            Map<String, String> env = System.getenv();
+            for(Class cl : classes) {
+                if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                    Field field = cl.getDeclaredField("m");
+                    field.setAccessible(true);
+                    Object obj = field.get(env);
+                    Map<String, String> map = (Map<String, String>) obj;
+                    map.clear();
+                    map.putAll(newenv);
+                }
+            }
+        }
+    }
+
     public static void setFinalStatic(Field field, Object newValue) throws Exception {
         field.setAccessible(true);
 
