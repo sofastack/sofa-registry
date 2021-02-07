f6e66a092b31f854970c03ea52ad36c49191e9ad
dzdx <dzidaxie@gmail.com>
2021-02-07 20:38:21 +0800

    fix rate dump

--- server/server/session/src/main/java/com/alipay/sofa/registry/server/session/scheduler/timertask/SessionCacheDigestTask.java
+++ server/server/session/src/main/java/com/alipay/sofa/registry/server/session/scheduler/timertask/SessionCacheDigestTask.java
@@ -26,22 +26,15 @@ import com.alipay.sofa.registry.server.session.bootstrap.SessionServerConfig;
 import com.alipay.sofa.registry.server.session.store.DataStore;
 import com.alipay.sofa.registry.server.session.store.Interests;
 import com.alipay.sofa.registry.util.ConcurrentUtils;
-import com.alipay.sofa.registry.util.NamedThreadFactory;
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
- *
  * @author xiaojian.xj
  * @version $Id: SessionCacheDigestTask.java, v 0.1 2020年08月03日 14:37 xiaojian.xj Exp $
  */
@@ -50,10 +43,10 @@ public class SessionCacheDigestTask {
     private static final Logger LOGGER = LoggerFactory.getLogger("CACHE-DIGEST");
 
     @Autowired
-    private DataStore           sessionDataStore;
+    private DataStore sessionDataStore;
 
     @Autowired
-    private Interests           sessionInterests;
+    private Interests sessionInterests;
 
     @Autowired
     private SessionServerConfig sessionServerConfig;
@@ -65,35 +58,44 @@ public class SessionCacheDigestTask {
             LOGGER.info("cache digest off with intervalSec={}", intervalSec);
             return;
         }
-        ScheduledExecutorService executor = new ScheduledThreadPoolExecutor(1,
-                new NamedThreadFactory("CacheDigestTask"));
-        executor.scheduleWithFixedDelay(() -> {
+        Date firstDate = new Date();
+        firstDate = DateUtils.round(firstDate, Calendar.MINUTE);
+        firstDate.setMinutes(firstDate.getMinutes() / 5 * 5 + 5);
+        Timer timer = new Timer("CacheDigestTask", true);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                dump();
+            }
+        };
+        timer.scheduleAtFixedRate(task, firstDate, intervalSec * 1000);
+    }
 
-                    try {
-                        Collection<String> storeDataInfoIds = sessionDataStore.getDataInfoIds();
-                        Collection<String> interestDataInfoIds = sessionInterests.getDataInfoIds();
-                        Set<String> dataInfoIds = new HashSet<>(storeDataInfoIds.size()+interestDataInfoIds.size());
+    private void dump() {
+        try {
+            Collection<String> storeDataInfoIds = sessionDataStore.getDataInfoIds();
+            Collection<String> interestDataInfoIds = sessionInterests.getDataInfoIds();
+            Set<String> dataInfoIds = new HashSet<>(storeDataInfoIds.size() + interestDataInfoIds.size());
 
-                        dataInfoIds.addAll(storeDataInfoIds);
-                        dataInfoIds.addAll(interestDataInfoIds);
+            dataInfoIds.addAll(storeDataInfoIds);
+            dataInfoIds.addAll(interestDataInfoIds);
 
-                        dataInfoIds.stream().forEach(dataInfoId -> {
-                            Collection<Publisher> publishers = sessionDataStore.getDatas(dataInfoId);
-                            Collection<Subscriber> subscribers = sessionInterests.getDatas(dataInfoId);
+            dataInfoIds.stream().forEach(dataInfoId -> {
+                Collection<Publisher> publishers = sessionDataStore.getDatas(dataInfoId);
+                Collection<Subscriber> subscribers = sessionInterests.getDatas(dataInfoId);
 
-                            LOGGER.info("[dataInfo] {}; {}; {}; {}; [{}]; [{}]",
-                                    sessionServerConfig.getSessionServerDataCenter(), dataInfoId,
-                                    publishers.size(), subscribers.size(),
-                                    logPubOrSub(publishers), logPubOrSub(subscribers));
-                            // avoid io is too busy
-                            ConcurrentUtils.sleepUninterruptibly(20, TimeUnit.MILLISECONDS);
-                        });
+                LOGGER.info("[dataInfo] {}; {}; {}; {}; [{}]; [{}]",
+                        sessionServerConfig.getSessionServerDataCenter(), dataInfoId,
+                        publishers.size(), subscribers.size(),
+                        logPubOrSub(publishers), logPubOrSub(subscribers));
+                // avoid io is too busy
+                ConcurrentUtils.sleepUninterruptibly(20, TimeUnit.MILLISECONDS);
+            });
 
-                    } catch (Throwable t) {
-                        LOGGER.error("[CacheDigestTask] cache digest error", t);
-                    }
+        } catch (Throwable t) {
+            LOGGER.error("[CacheDigestTask] cache digest error", t);
+        }
 
-                }, intervalSec, intervalSec, TimeUnit.SECONDS);
     }
 
     private String logPubOrSub(Collection<? extends BaseInfo> infos) {
