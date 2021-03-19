63ef3d157915f3f94db110a886aebc5d97ff12c9
忘禅 <zhuchen.zhu@alibaba-inc.com>
2021-03-19 22:55:10 +0800

    reformat

--- server/server/meta/src/main/java/com/alipay/sofa/registry/server/meta/monitor/impl/DefaultSlotTableStats.java
+++ server/server/meta/src/main/java/com/alipay/sofa/registry/server/meta/monitor/impl/DefaultSlotTableStats.java
@@ -26,7 +26,6 @@ import com.alipay.sofa.registry.server.meta.monitor.SlotTableStats;
 import com.alipay.sofa.registry.server.meta.slot.SlotManager;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Maps;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -36,157 +35,161 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author chen.zhu
- * <p>
- * Jan 28, 2021
+ *     <p>Jan 28, 2021
  */
 public class DefaultSlotTableStats extends AbstractLifecycle implements SlotTableStats {
 
-    private final SlotManager             slotManager;
+  private final SlotManager slotManager;
 
-    private final Map<Integer, SlotStats> slotStatses = Maps.newConcurrentMap();
+  private final Map<Integer, SlotStats> slotStatses = Maps.newConcurrentMap();
 
-    private final ReadWriteLock           lock        = new ReentrantReadWriteLock();
+  private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
-    public DefaultSlotTableStats(SlotManager slotManager) {
-        this.slotManager = slotManager;
+  public DefaultSlotTableStats(SlotManager slotManager) {
+    this.slotManager = slotManager;
+  }
+
+  @Override
+  protected void doInitialize() throws InitializeException {
+    super.doInitialize();
+    for (int slotId = 0; slotId < SlotConfig.SLOT_NUM; slotId++) {
+      Slot slot = slotManager.getSlotTable().getSlot(slotId);
+      if (slot == null) {
+        slot = new Slot(slotId, null, 0L, Collections.emptyList());
+      }
+      slotStatses.put(slotId, new DefaultSlotStats(slot));
     }
+  }
 
-    @Override
-    protected void doInitialize() throws InitializeException {
-        super.doInitialize();
-        for (int slotId = 0; slotId < SlotConfig.SLOT_NUM; slotId++) {
-            Slot slot = slotManager.getSlotTable().getSlot(slotId);
-            if (slot == null) {
-                slot = new Slot(slotId, null, 0L, Collections.emptyList());
-            }
-            slotStatses.put(slotId, new DefaultSlotStats(slot));
+  @Override
+  public boolean isSlotLeadersStable() {
+    try {
+      lock.readLock().lock();
+      for (int slotId = 0; slotId < SlotConfig.SLOT_NUM; slotId++) {
+        String leader = slotManager.getSlotTable().getSlot(slotId).getLeader();
+        SlotStats slotStats = slotStatses.get(slotId);
+        if (!slotStats.getSlot().getLeader().equals(leader)
+            || !slotStatses.get(slotId).isLeaderStable()) {
+          logger.warn("[isSlotLeadersStable]slot[{}] leader[{}] not stable", slotId, leader);
+          return false;
         }
+      }
+      return true;
+    } finally {
+      lock.readLock().unlock();
     }
+  }
 
-    @Override
-    public boolean isSlotLeadersStable() {
-        try {
-            lock.readLock().lock();
-            for (int slotId = 0; slotId < SlotConfig.SLOT_NUM; slotId++) {
-                String leader = slotManager.getSlotTable().getSlot(slotId).getLeader();
-                SlotStats slotStats = slotStatses.get(slotId);
-                if (!slotStats.getSlot().getLeader().equals(leader)
-                    || !slotStatses.get(slotId).isLeaderStable()) {
-                    logger.warn("[isSlotLeadersStable]slot[{}] leader[{}] not stable", slotId,
-                        leader);
-                    return false;
-                }
-            }
-            return true;
-        } finally {
-            lock.readLock().unlock();
+  @Override
+  public boolean isSlotFollowersStable() {
+    try {
+      lock.readLock().lock();
+      for (Map.Entry<Integer, SlotStats> entry : slotStatses.entrySet()) {
+        Set<String> followers = slotManager.getSlotTable().getSlot(entry.getKey()).getFollowers();
+        for (String follower : followers) {
+          if (!entry.getValue().isFollowerStable(follower)) {
+            logger.warn(
+                "[isSlotFollowersStable]slot[{}] follower not stable {}",
+                entry.getKey(),
+                entry.getValue());
+            return false;
+          }
         }
+      }
+      return true;
+    } finally {
+      lock.readLock().unlock();
     }
+  }
 
-    @Override
-    public boolean isSlotFollowersStable() {
-        try {
-            lock.readLock().lock();
-            for (Map.Entry<Integer, SlotStats> entry : slotStatses.entrySet()) {
-                Set<String> followers = slotManager.getSlotTable().getSlot(entry.getKey())
-                    .getFollowers();
-                for (String follower : followers) {
-                    if (!entry.getValue().isFollowerStable(follower)) {
-                        logger.warn("[isSlotFollowersStable]slot[{}] follower not stable {}",
-                            entry.getKey(), entry.getValue());
-                        return false;
-                    }
-                }
-            }
-            return true;
-        } finally {
-            lock.readLock().unlock();
+  @Override
+  public void checkSlotStatuses(DataNode node, List<BaseSlotStatus> slotStatuses) {
+    try {
+      lock.writeLock().lock();
+      for (BaseSlotStatus slotStatus : slotStatuses) {
+        int slotId = slotStatus.getSlotId();
+        SlotStats slotStats = slotStatses.get(slotId);
+        if (slotStats == null || slotStats.getSlot() == null) {
+          continue;
         }
-    }
-
-    @Override
-    public void checkSlotStatuses(DataNode node, List<BaseSlotStatus> slotStatuses) {
-        try {
-            lock.writeLock().lock();
-            for (BaseSlotStatus slotStatus : slotStatuses) {
-                int slotId = slotStatus.getSlotId();
-                SlotStats slotStats = slotStatses.get(slotId);
-                if (slotStats == null || slotStats.getSlot() == null) {
-                    continue;
-                }
-                if (slotStats.getSlot().getLeaderEpoch() > slotStatus.getSlotLeaderEpoch()) {
-                    logger
-                        .warn(
-                            "[checkSlotStatuses] won't update slot status, slot[{}] leader-epoch[{}] is less than current[{}]",
-                            slotId, slotStatus.getSlotLeaderEpoch(), slotStats.getSlot()
-                                .getLeaderEpoch());
-                    continue;
-                } else if (slotStats.getSlot().getLeaderEpoch() < slotStatus.getSlotLeaderEpoch()) {
-                    Metrics.DataSlot.setDataSlotGreaterThanMeta(node.getIp(), slotId);
-                    logger
-                        .error(
-                            "[checkSlotStatuses] won't update slot status, slot[{}] leader-epoch[{}] reported by data({}) is more than current[{}]",
-                            slotId, slotStatus.getSlotLeaderEpoch(), node.getIp(), slotStats
-                                .getSlot().getLeaderEpoch());
-                    continue;
-                }
-                if (!slotStats.getSlot().equals(slotManager.getSlotTable().getSlot(slotId))) {
-                    logger
-                        .error(
-                            "[checkSlotStatuses] slot reported by data({}) is not equals with mine({}), not update",
-                            slotStats.getSlot(), slotManager.getSlotTable().getSlot(slotId));
-                    continue;
-                }
-
-                if (slotStatus.getRole() == Slot.Role.Leader) {
-                    if (!slotStats.getSlot().getLeader().equals(node.getIp())) {
-                        logger
-                            .error(
-                                "[checkSlotStatuses] slot leader({}) is not equal with reported data-server({})",
-                                slotStats.getSlot().getLeader(), node.getIp());
-                        Metrics.DataSlot.setDataReportNotStable(node.getIp(), slotId);
-                        continue;
-                    }
-                    slotStats.updateLeaderState((LeaderSlotStatus) slotStatus);
-                } else {
-                    if (!slotStats.getSlot().getFollowers().contains(node.getIp())) {
-                        logger
-                            .error(
-                                "[checkSlotStatuses] slot follower({}) is not containing reported data-server({})",
-                                slotStats.getSlot().getFollowers(), node.getIp());
-                        Metrics.DataSlot.setDataReportNotStable(node.getIp(), slotId);
-                        continue;
-                    }
-                    slotStats.updateFollowerState((FollowerSlotStatus) slotStatus);
-                }
-            }
-        } finally {
-            lock.writeLock().unlock();
+        if (slotStats.getSlot().getLeaderEpoch() > slotStatus.getSlotLeaderEpoch()) {
+          logger.warn(
+              "[checkSlotStatuses] won't update slot status, slot[{}] leader-epoch[{}] is less than current[{}]",
+              slotId,
+              slotStatus.getSlotLeaderEpoch(),
+              slotStats.getSlot().getLeaderEpoch());
+          continue;
+        } else if (slotStats.getSlot().getLeaderEpoch() < slotStatus.getSlotLeaderEpoch()) {
+          Metrics.DataSlot.setDataSlotGreaterThanMeta(node.getIp(), slotId);
+          logger.error(
+              "[checkSlotStatuses] won't update slot status, slot[{}] leader-epoch[{}] reported by data({}) is more than current[{}]",
+              slotId,
+              slotStatus.getSlotLeaderEpoch(),
+              node.getIp(),
+              slotStats.getSlot().getLeaderEpoch());
+          continue;
         }
-    }
+        if (!slotStats.getSlot().equals(slotManager.getSlotTable().getSlot(slotId))) {
+          logger.error(
+              "[checkSlotStatuses] slot reported by data({}) is not equals with mine({}), not update",
+              slotStats.getSlot(),
+              slotManager.getSlotTable().getSlot(slotId));
+          continue;
+        }
 
-    @Override
-    public void updateSlotTable(SlotTable slotTable) {
-        try {
-            lock.writeLock().lock();
-            logger.info("[updateSlotTable] update slot table for epoch [{}]", slotTable.getEpoch());
-            slotTable.getSlotMap().forEach((slotId, slot) -> {
+        if (slotStatus.getRole() == Slot.Role.Leader) {
+          if (!slotStats.getSlot().getLeader().equals(node.getIp())) {
+            logger.error(
+                "[checkSlotStatuses] slot leader({}) is not equal with reported data-server({})",
+                slotStats.getSlot().getLeader(),
+                node.getIp());
+            Metrics.DataSlot.setDataReportNotStable(node.getIp(), slotId);
+            continue;
+          }
+          slotStats.updateLeaderState((LeaderSlotStatus) slotStatus);
+        } else {
+          if (!slotStats.getSlot().getFollowers().contains(node.getIp())) {
+            logger.error(
+                "[checkSlotStatuses] slot follower({}) is not containing reported data-server({})",
+                slotStats.getSlot().getFollowers(),
+                node.getIp());
+            Metrics.DataSlot.setDataReportNotStable(node.getIp(), slotId);
+            continue;
+          }
+          slotStats.updateFollowerState((FollowerSlotStatus) slotStatus);
+        }
+      }
+    } finally {
+      lock.writeLock().unlock();
+    }
+  }
+
+  @Override
+  public void updateSlotTable(SlotTable slotTable) {
+    try {
+      lock.writeLock().lock();
+      logger.info("[updateSlotTable] update slot table for epoch [{}]", slotTable.getEpoch());
+      slotTable
+          .getSlotMap()
+          .forEach(
+              (slotId, slot) -> {
                 SlotStats slotStats = slotStatses.get(slotId);
                 if (slotStats.getSlot().getLeaderEpoch() < slot.getLeaderEpoch()) {
-                    slotStatses.put(slotId, new DefaultSlotStats(slot));
-                } else if (slotStats.getSlot().getLeaderEpoch() == slot.getLeaderEpoch() && !slotStats.getSlot().equals(slot)) {
-                    slotStatses.put(slotId, new DefaultSlotStats(slot));
+                  slotStatses.put(slotId, new DefaultSlotStats(slot));
+                } else if (slotStats.getSlot().getLeaderEpoch() == slot.getLeaderEpoch()
+                    && !slotStats.getSlot().equals(slot)) {
+                  slotStatses.put(slotId, new DefaultSlotStats(slot));
                 } else {
-                    logger.warn("[updateSlotTable]skip slot[{}]", slotId);
+                  logger.warn("[updateSlotTable]skip slot[{}]", slotId);
                 }
-            });
-        } finally {
-            lock.writeLock().unlock();
-        }
+              });
+    } finally {
+      lock.writeLock().unlock();
     }
+  }
 
-    @VisibleForTesting
-    public SlotStats getSlotStats(int slotId) {
-        return slotStatses.get(slotId);
-    }
+  @VisibleForTesting
+  public SlotStats getSlotStats(int slotId) {
+    return slotStatses.get(slotId);
+  }
 }
