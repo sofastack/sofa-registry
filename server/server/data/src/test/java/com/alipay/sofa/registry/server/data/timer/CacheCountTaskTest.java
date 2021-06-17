3507a7396fbb4040cade767b6990bc3bb5018fa7
yuzhi.lyz <yuzhi.lyz@antfin.com>
2021-06-17 09:54:47 +0800

    support minute-level statistics

--- server/server/data/src/test/java/com/alipay/sofa/registry/server/data/timer/CacheCountTaskTest.java
+++ server/server/data/src/test/java/com/alipay/sofa/registry/server/data/timer/CacheCountTaskTest.java
@@ -41,6 +41,7 @@ public class CacheCountTaskTest {
     Assert.assertFalse(task.init());
     // empty
     Assert.assertTrue(task.count());
+    Assert.assertFalse(task.printTotal());
 
     cfg.setCacheCountIntervalSecs(1);
     Publisher pub = TestBaseUtils.createTestPublisher("testDataId");
@@ -48,5 +49,6 @@ public class CacheCountTaskTest {
     // has item
     Assert.assertTrue(task.count());
     Assert.assertTrue(task.init());
+    Assert.assertTrue(task.printTotal());
   }
 }
