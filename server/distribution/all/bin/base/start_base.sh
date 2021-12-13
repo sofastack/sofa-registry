a979c52ca0ef7ac299fcc4cf5e2c658e500d8d9c
dzdx <dzidaxie@gmail.com>
2021-12-13 20:57:30 +0800

    deploy config

--- server/distribution/all/bin/base/start_base.sh
+++ server/distribution/all/bin/base/start_base.sh
@@ -7,13 +7,11 @@ if [[ -z "$REGISTRY_APP_NAME" ]]; then
     echo "Must provide REGISTRY_APP_NAME in environment" 1>&2
     exit 1
 fi
-BASE_DIR=`cd $(dirname $0)/../..; pwd`
-APP_JAR="${BASE_DIR}/registry-${REGISTRY_APP_NAME}.jar"
-# set user.home
-JAVA_OPTS="$JAVA_OPTS -Duser.home=${BASE_DIR}"
+RELEASE_DIR=`cd $(dirname $0)/../..; pwd`
+APP_JAR="${RELEASE_DIR}/registry-${REGISTRY_APP_NAME}.jar"
 
 if [[ -z "$SPRING_CONFIG_LOCATION" ]]; then
-  SPRING_CONFIG_LOCATION=${BASE_DIR}/conf/
+  SPRING_CONFIG_LOCATION=${RELEASE_DIR}/conf/
 fi
 JAVA_OPTS="$JAVA_OPTS -Dspring.config.additional-location=${SPRING_CONFIG_LOCATION}"
 
@@ -24,7 +22,7 @@ fi
 
 # springboot conf
 SPRINGBOOT_OPTS="${SPRINGBOOT_OPTS} -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
-SPRINGBOOT_OPTS="-Dlogging.config=${BASE_DIR}/conf/${REGISTRY_APP_NAME}/log4j2.xml  -Dlog4j.configurationFile=${BASE_DIR}/conf/${REGISTRY_APP_NAME}/log4j2.xml"
+SPRINGBOOT_OPTS="-Dlogging.config=${RELEASE_DIR}/log-conf/${REGISTRY_APP_NAME}/log4j2.xml  -Dlog4j.configurationFile=${RELEASE_DIR}/log-conf/${REGISTRY_APP_NAME}/log4j2.xml"
 
 MEM=8192
 HEAP_MAX=$(expr $MEM \* 60 / 100)
@@ -33,9 +31,9 @@ DIRECT_MEM=$(expr $MEM \* 10 / 100)
 JAVA_OPTS="$JAVA_OPTS -server -Xms${HEAP_MAX}m -Xmx${HEAP_MAX}m -Xmn${HEAP_NEW}m -Xss512k -XX:MetaspaceSize=150m -XX:MaxMetaspaceSize=150m "
 
 # gc option
-mkdir -p "${BASE_DIR}/logs"
-JAVA_OPTS="$JAVA_OPTS -XX:+DisableExplicitGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:${BASE_DIR}/logs/registry-${REGISTRY_APP_NAME}-gc.log -verbose:gc"
-JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs -XX:ErrorFile=${BASE_DIR}/logs/registry-${REGISTRY_APP_NAME}-hs_err_pid%p.log"
+mkdir -p "${HOME}/logs"
+JAVA_OPTS="$JAVA_OPTS -XX:+DisableExplicitGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:${HOME}/logs/registry-${REGISTRY_APP_NAME}-gc.log -verbose:gc"
+JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${HOME}/logs -XX:ErrorFile=${HOME}/logs/registry-${REGISTRY_APP_NAME}-hs_err_pid%p.log"
 JAVA_OPTS="$JAVA_OPTS -XX:-OmitStackTraceInFastThrow -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:ParallelGCThreads=4"
 JAVA_OPTS="$JAVA_OPTS -XX:+CMSClassUnloadingEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70"
 if [[ -n "$DEBUG_PORT" ]]; then
